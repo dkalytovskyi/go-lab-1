c91283c720e4e75157cb41180dfef7dfa363fe12
Denis Kalytovskyi <dkalitovskyi@gmail.com>
2020-02-24 17:17:11 +0200

    Fail the first test

--- implementation_test.go
+++ implementation_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -16,7 +17,7 @@ var _ = Suite(&MySuite{})
 func (s *MySuite) ExamplePrefixToPostfix(c *C) {
 	res, err := PrefixToPostfix("* + 22 45 - 3 17")
 	c.Check(err, Equals, nil)
-    c.Check(res, Equals, "22 45 + 3 17 - *")
+	c.Check(res, Equals, "22 4 + 3 17 - *")
 }
 
 /* Simple test cases */
@@ -24,13 +25,13 @@ func (s *MySuite) ExamplePrefixToPostfix(c *C) {
 func (s *MySuite) TestPrefixToPostfixSimpleFirst(c *C) {
 	res, err := PrefixToPostfix("- * 3 5 / 7 6")
 	c.Check(err, Equals, nil)
-    c.Check(res, Equals, "3 5 * 7 6 / -")
+	c.Check(res, Equals, "3 5 * 7 6 / -")
 }
 
 func (s *MySuite) TestPrefixToPostfixSimpleSecond(c *C) {
 	res, err := PrefixToPostfix("+ ^ 3 2 + 5 6")
 	c.Check(err, Equals, nil)
-    c.Check(res, Equals, "3 2 ^ 5 6 + +")
+	c.Check(res, Equals, "3 2 ^ 5 6 + +")
 }
 
 /* Complicated test cases */
@@ -38,33 +39,33 @@ func (s *MySuite) TestPrefixToPostfixSimpleSecond(c *C) {
 func (s *MySuite) TestPrefixToPostfixComplicatedFirst(c *C) {
 	res, err := PrefixToPostfix("- / * 4 8 22 - - ^ 8 4 1 + 3 * 54 3")
 	c.Check(err, Equals, nil)
-    c.Check(res, Equals, "4 8 * 22 / 8 4 ^ 1 - 3 54 3 * + - -")
+	c.Check(res, Equals, "4 8 * 22 / 8 4 ^ 1 - 3 54 3 * + - -")
 }
 
 func (s *MySuite) TestPrefixToPostfixComplicatedSecond(c *C) {
 	res, err := PrefixToPostfix("/ 3 + + * * 3 1 4 3 - ^ 7 2 * 1 9")
 	c.Check(err, Equals, nil)
-    c.Check(res, Equals, "3 3 1 * 4 * 3 + 7 2 ^ 1 9 * - + /")
+	c.Check(res, Equals, "3 3 1 * 4 * 3 + 7 2 ^ 1 9 * - + /")
 }
 
 /* Error test cases */
 
 func (s *MySuite) TestPrefixToPostfixErrorEmptyLine(c *C) {
 	_, err := PrefixToPostfix("  ")
-	c. Check(err.Error(), Equals, "Error: empty line entered!")
+	c.Check(err.Error(), Equals, "Error: empty line entered!")
 }
 
 func (s *MySuite) TestPrefixToPostfixErrorInvalidOperand(c *C) {
 	_, err := PrefixToPostfix("- * e 5 / 7 6")
-	c. Check(err.Error(), Equals, "Error: operand is not valid!")
+	c.Check(err.Error(), Equals, "Error: operand is not valid!")
 }
 
 func (s *MySuite) TestPrefixToPostfixErrorFewOperands(c *C) {
 	_, err := PrefixToPostfix("- 2")
-	c. Check(err.Error(), Equals,"Error: not enough operands!")
+	c.Check(err.Error(), Equals, "Error: not enough operands!")
 }
 
 func (s *MySuite) TestPrefixToPostfixErrorFewOperators(c *C) {
 	_, err := PrefixToPostfix("- 2 7 5")
-	c. Check(err.Error(), Equals,"Error: input is not valid")
+	c.Check(err.Error(), Equals, "Error: input is not valid")
 }
